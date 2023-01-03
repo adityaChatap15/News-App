bcbd37d0255afda0d1af985263e34f22620d8482
Aditya Chatap <adityachatap123@gmail.com>
2023-01-03 22:39:59 +0530

    added the bootstrap and started adding some functions of news api

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -4,9 +4,21 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news Component
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-3">
+            <NewsItem title="myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-3">
+            <NewsItem title="myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-3">
+            <NewsItem title="myTitle" description="mydesc" />
+          </div>
+        
+        </div>
+        
       </div>
     )
   }
